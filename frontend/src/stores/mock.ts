c3ef93488a8e170ba60029085c030b3450fc28a3
Tuong Le <tuongaz@gmail.com>
2022-07-07 22:57:40 +1000

    list mocks

--- frontend/src/stores/mock.ts
+++ frontend/src/stores/mock.ts
@@ -1,10 +1,30 @@
 import {defineStore} from 'pinia'
 import axios from 'axios'
 
-export interface Mock {
+export enum Status {
+    STOPPED = "stopped",
+    RUNNING = "running"
+}
+
+export interface MockData {
     id: string
     name: string
+    description: string
     routes: Route[]
+}
+
+export interface MockState {
+    url?: string
+    status: Status
+}
+
+export interface Mock {
+    data: MockData
+    state: MockState
+}
+
+export interface Mocks {
+    [id: string]: Mock
 }
 
 export interface Route {
@@ -20,23 +40,50 @@ export interface Response {
 export const useMockStore = defineStore({
     id: "mocks",
     state: () => ({
-        mocks: [] as Mock[],
+        activeId: null as string | null,
+        mocks: {} as Mocks,
         error: null as any,
     }),
     getters: {
-        getMocks: (state) => state.mocks
+        getMockByID(state) {
+            return (id: string): Mock | undefined => state.mocks[id]
+        },
+        activeMock(state): Mock | undefined {
+            if (!!state.activeId) {
+                return this.getMockByID(state.activeId)
+            }
+
+            const ids = Object.keys(state.mocks)
+            if (ids.length === 0) {
+                return undefined
+            }
+
+            this.activeId = ids[0]
+            return this.getMockByID(this.activeId)
+        }
     },
     actions: {
+        setActiveMock(id: string) {
+            this.activeId = id
+        },
         async fetchMocks() {
             try {
-                const {data} = await axios.get<Mock[]>(getUrl("/mocks"))
-                this.mocks = data
+                const {data} = await axios.get<MockData[]>(getUrl("/mocks"))
+                this.mocks = data.reduce((acc, mock) => {
+                    acc[mock.id] = {
+                        data: mock,
+                        state: {
+                            status: Status.STOPPED
+                        }
+                    }
+                    return acc
+                }, {} as Mocks)
             } catch (error) {
                 this.error = {
                     error,
                 }
             }
-        }
+        },
     }
 })
 
