9d6c82e61cf3a9d51e06cff5cfa497c2f5734475
Tuong Le <tuongaz@gmail.com>
2022-07-14 00:14:31 +1000

    add tabs for response

--- frontend/src/components/mock/response/Response.vue
+++ frontend/src/components/mock/response/Response.vue
@@ -1,23 +1,76 @@
 <template>
   <div>
-    <div class="flex dark:bg-slate-800 bg-white">
+    <div @click="toggleOpen" class="select-none flex dark:bg-slate-800 bg-white cursor-pointer">
       <div class="m-5 block flex-1 flex justify-between">
-        <p>{{ response.status }}</p>
+        <h4>{{ response.status }}</h4>
         <ViewListIcon class="handle w-5 h-5 ml-5"/>
       </div>
     </div>
 
-    <div class="hidden">
-      Detail here
+    <div :class="open ? 'block' : 'hidden'">
+      <div class="border-b border-gray-200 dark:border-slate-700 mx-5">
+        <TabGroup>
+          <div class="border-b border-gray-200 dark:border-slate-700">
+            <TabList class="-mb-px flex space-x-8">
+              <Tab v-for="item in tabs" :key="item.name" v-slot="{ selected }" as="template">
+                <button
+                    :class="[selected ? 'border-green-600': 'border-transparent', 'focus:outline-none whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm hover:border-green-600']">
+                  {{ item.name }}
+                </button>
+              </Tab>
+            </TabList>
+          </div>
+          <TabPanels>
+            <TabPanel v-for="item in tabs" :key="item.name">
+              <div class="my-5">
+                <component :is="item.component"/>
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </TabGroup>
+        <div class="my-5 flex justify-end">
+          <button type="button"
+                  class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-red-500 border-red-500 hover:text-white hover:bg-red-500">
+            Delete
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
 import type {Response} from "@/stores";
 import {ViewListIcon} from '@heroicons/vue/outline';
+import {Tab, TabGroup, TabList, TabPanel, TabPanels} from '@headlessui/vue'
+import Body from "@/components/mock/response/Body.vue";
+import Headers from "@/components/mock/response/Headers.vue";
+import Rules from "@/components/mock/rule/Rules.vue";
+
+const tabs = [
+  {
+    name: 'Status & Body',
+    component: Body,
+  },
+  {
+    name: 'Headers',
+    component: Headers,
+  },
+  {
+    name: 'Rules',
+    component: Rules,
+  }
+];
 
 defineProps({
   response: {type: Object as () => Response, required: true}
 })
+
+const open = ref(false)
+
+function toggleOpen() {
+  open.value = !open.value
+}
 </script>
+
