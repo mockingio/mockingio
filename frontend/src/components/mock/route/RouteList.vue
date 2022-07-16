681b320b785a680978001cf50a26e2f0ecd02f3d
Tuong Le <tuongaz@gmail.com>
2022-07-16 23:24:11 +1000

    restructure

--- frontend/src/components/mock/route/RouteList.vue
+++ frontend/src/components/mock/route/RouteList.vue
@@ -1,15 +1,4 @@
 <template>
-  <form class="sm:flex sm:items-center m-2">
-    <div class="w-full sm:max-w-xs relative">
-      <input type="email" name="email" id="email"
-             class="shadow-sm block w-full sm:text-sm border-gray-300 dark:border-slate-700 rounded-md dark:bg-slate-800"
-             placeholder="find route"/>
-      <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-        <FilterIcon class="h-5 w-5 dark:text-slate-500 text-gray-400" aria-hidden="true"/>
-      </div>
-    </div>
-    <PlusIcon class="w-5 h-5 ml-2 text-green-500"/>
-  </form>
   <nav class="space-y-1" v-if="activeMock">
     <RouteListItem v-for="route in activeMock.data.routes" :route="route" :mock="activeMock"
                    :key="route.id" :request="route.request"/>
@@ -17,7 +6,6 @@
 </template>
 
 <script setup lang="ts">
-import {FilterIcon, PlusIcon} from '@heroicons/vue/outline';
 import type {Mock} from "@/stores";
 import RouteListItem from "./RouteListItem.vue";
 
