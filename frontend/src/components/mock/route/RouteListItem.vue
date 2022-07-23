36cd9e5ab644697be17ded8cc3f4527535cb1a55
Tuong Le <tuongaz@gmail.com>
2022-07-23 14:25:56 +1000

    feat: move option to right

--- frontend/src/components/mock/route/RouteListItem.vue
+++ frontend/src/components/mock/route/RouteListItem.vue
@@ -5,11 +5,11 @@
     <div
         :class="[isActive ? 'bg-slate-800' : 'border-transparent', 'hover:bg-slate-800 flex items-center group block text-sm py-1']">
       <div class="pl-3 pr-1 py-1 flex-1">
-      <span class="flex items-center justify-between">
-          <span>{{ route.path }}</span>
-          <span :class="`rounded p-1 text-white method-${route.method.toLowerCase()} text-xs`">{{
-              route.method
+      <span class="flex items-center">
+          <span :class="`rounded p-1 text-white w-[40px] method-${route.method.toLowerCase()} text-xxs`">{{
+              shortMethod
             }}</span>
+          <span>{{ route.path }}</span>
       </span>
         <p class="text-xs my-1 text-gray-500 dark:text-slate-600">
           {{ route.description }}
@@ -26,10 +26,24 @@
 <script setup lang="ts">
 import type {Mock, Route} from "@/stores";
 import PopoverMenu from "@/components/ui/PopoverMenu.vue";
+import {computed} from "vue";
 
 const props = defineProps({
   route: {type: Object as () => Route, required: true},
   mock: {type: Object as () => Mock, required: true},
+})
+
+const shortMethodMap: { [key: string]: string } = {
+  POST: "POST",
+  GET: "GET",
+  PUT: "PUT",
+  PATCH: "PATCH",
+  DELETE: "DEL",
+  OPTIONS: "OPT"
+}
+
+const shortMethod = computed(() => {
+  return shortMethodMap[props.route.method]
 })
 
 const items = [
