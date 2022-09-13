6dcd01da5cce4fe1d5cf415427cf4a91b3531cad
Tuong Le <tuongaz@gmail.com>
2022-09-13 22:43:02 +1000

    support faker

---
+++ engine/plugins/faker/faker_test.go
@@ -0,0 +1,93 @@
+package faker
+
+import (
+	"testing"
+
+	"github.com/mockingio/mockingio/engine/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseCommand(t *testing.T) {
+	tests := []struct {
+		input            string
+		expectedCommands []commandTuple
+	}{
+		{
+			"something {{faker.person.name}} here {{faker.lorem.text(100)}} example",
+			[]commandTuple{
+				{
+					placeholder: "{{faker.person.name}}",
+					command: command{
+						"person": command{
+							"name": nil,
+						},
+					},
+				},
+				{
+					placeholder: "{{faker.lorem.text(100)}}",
+					command: command{
+						"lorem": command{
+							"text": []any{100},
+						},
+					},
+				},
+			},
+		},
+		{
+			"{{faker.person.name}}",
+			[]commandTuple{
+				{
+					placeholder: "{{faker.person.name}}",
+					command: command{
+						"person": command{
+							"name": nil,
+						},
+					},
+				},
+			},
+		},
+		{
+			"{{faker.person.name('alex')}}",
+			[]commandTuple{
+				{
+					placeholder: "{{faker.person.name('alex')}}",
+					command: command{
+						"person": command{
+							"name": []any{"alex"},
+						},
+					},
+				},
+			},
+		},
+		{
+			"no commands",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		commands, err := parseCommand(tt.input)
+		require.NoError(t, err)
+		assert.Equal(t, tt.expectedCommands, commands)
+	}
+}
+
+func TestFaker_Response(t *testing.T) {
+	plug := New()
+
+	resp := &mock.Response{
+		Body: "hi {{faker.person.name}}",
+		Headers: map[string]string{
+			"X-Name": "token: {{faker.lorem.text(5)}}",
+			"X-Test": "no commands",
+		},
+	}
+	plug.Response(resp)
+
+	assert.NotEqual(t, "hi {{faker.person.name}}", resp.Body)
+	assert.NotEqual(t, map[string]string{
+		"X-Name": "token: {{faker.lorem.text(5)}}",
+		"X-Test": "no commands",
+	}, resp.Headers)
+}
---
+++ engine/plugins/faker/parser.go
@@ -0,0 +1,109 @@
+package faker
+
+import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type command map[string]any
+type commandTuple struct {
+	placeholder string
+	command     command
+}
+
+// parseCommand parse the string and get commands
+func parseCommand(input string) ([]commandTuple, error) {
+	var commands []commandTuple
+	comp, err := regexp.Compile(`{{faker\.([^{}]+)}}`)
+	if err != nil {
+		return nil, err
+	}
+
+	groups := comp.FindAllStringSubmatch(input, -1)
+	if len(groups) == 0 {
+		return nil, nil
+	}
+
+	for _, group := range groups {
+		if len(group) != 2 {
+			continue
+		}
+
+		cmd, err := toCommand(strings.Split(group[1], "."))
+		if err != nil {
+			return nil, err
+		}
+		commands = append(commands, commandTuple{
+			placeholder: group[0],
+			command:     cmd,
+		})
+	}
+
+	return commands, nil
+}
+
+func toArguments(args []string) ([]any, error) {
+	var arguments []any
+	for _, arg := range args {
+		matched, err := regexp.Match(`^['"]+.*['"]+$`, []byte(arg))
+		if err != nil {
+			return nil, err
+		}
+
+		if matched {
+			arguments = append(arguments, strings.Trim(arg, `"'`))
+			continue
+		}
+
+		// if not matched, assume it's a number
+		intVar, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+
+		arguments = append(arguments, intVar)
+	}
+
+	return arguments, nil
+}
+
+func toCommand(tokens []string) (command, error) {
+	if len(tokens) == 1 {
+		token := tokens[0]
+		if strings.Contains(token, "(") {
+			comp, err := regexp.Compile(`([^(]+)\(([^)]+)\)`)
+			if err != nil {
+				return command{}, err
+			}
+
+			groups := comp.FindAllStringSubmatch(token, -1)
+			if len(groups) != 1 {
+				return command{}, errors.New("invalid command")
+			}
+
+			args, err := toArguments(groups[0][2:])
+			if err != nil {
+				return command{}, err
+			}
+
+			return command{
+				groups[0][1]: args,
+			}, nil
+		}
+
+		return command{
+			token: nil,
+		}, nil
+	}
+
+	newCmd, err := toCommand(tokens[1:])
+	if err != nil {
+		return command{}, err
+	}
+
+	return command{
+		tokens[0]: newCmd,
+	}, nil
+}
